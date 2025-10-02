2cd612cbdf5b591d05de6b448ea26d1e1b2693da
Maksim Montagne <maxime@drosalys.fr>
2025-10-02 14:01:28 +0200

    using lifespan instead of deprecated events

--- app/main.py
+++ app/main.py
@@ -1,23 +1,29 @@
 from fastapi import FastAPI
 import redis.asyncio as redis
+from contextlib import asynccontextmanager
 
-app = FastAPI()
-r = redis.Redis(host='localhost', port=6379, decode_responses=True)
+r: redis.Redis|None
 
-@app.on_event("startup")
-async def startup_event():
-    # Check connection on startup
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    global r
+    r = redis.Redis(host='localhost', port=6379, decode_responses=True)
     try:
         await r.ping()
         print("Connected to Redis")
     except Exception as e:
         print("Redis connection failed:", e)
 
+    yield  # Application runs here
+
+    # Shutdown
+    if r:
+        await r.close()
+        print("Redis connection closed")
+
+app = FastAPI(lifespan=lifespan)
+
+
 @app.get("/")
 async def read_root():
     return {"Hello": "World"}
-
-@app.get("/ping")
-async def ping():
-    return await r.ping()
-
